65c634f1896b7e809a4daf78308aeec3f53f5dfa
AlfredhoPranoto <alfredhopranoto@gmail.com>
2025-03-20 04:26:50 +0700

    testes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 app.use(express.json()); // allow us to accept JSON data in the req.body
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
