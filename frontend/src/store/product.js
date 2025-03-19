77b382d031c54a7b956e4e40aedbca20ae9c72c7
AlfredhoPranoto <alfredhopranoto@gmail.com>
2025-03-20 04:22:52 +0700

    tes

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 export const useProductStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
@@ -7,7 +7,7 @@ export const useProductStore = create((set) => ({
     if (!newProduct.name || !newProduct.image || !newProduct.price) {
       return { success: false, message: "Please fill in all fields" };
     }
-    const res = await fetch("/api/products", {
+    const res = await fetch(`${BASE_URL}/api/products`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -19,12 +19,12 @@ export const useProductStore = create((set) => ({
     return { success: true, message: "Product created successfully" };
   },
   fetchProducts: async () => {
-    const res = await fetch("/api/products");
+    const res = await fetch(`${BASE_URL}/api/products`);
     const data = await res.json();
     set({ products: data.data });
   },
   deleteProduct: async (id) => {
-    const res = await fetch(`/api/products/${id}`, {
+    const res = await fetch(`${BASE_URL}/api/products/${id}`, {
       method: "DELETE",
     });
     const data = await res.json();
@@ -38,7 +38,7 @@ export const useProductStore = create((set) => ({
     return { success: true, message: data.message };
   },
   updateProduct: async (id, updatedProduct) => {
-    const res = await fetch(`/api/products/${id}`, {
+    const res = await fetch(`${BASE_URL}/api/products/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,13 +6,6 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_BACKEND_URL || "http://localhost:5000",
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Tambahkan ini
