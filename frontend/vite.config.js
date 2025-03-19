1f4f33d8cc2d366bc703b6ff26a07a0f9cf465a6
AlfredhoPranoto <alfredhopranoto@gmail.com>
2025-03-20 01:16:56 +0700

    Bug fix: vite config ourDir

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Pastikan output ke dist
+  },
   server: {
     proxy: {
       "/api": {
