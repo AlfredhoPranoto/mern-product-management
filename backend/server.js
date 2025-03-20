eaaa1d157d31bb23566cdeaea6b377fcbb21063e
AlfredhoPranoto <alfredhopranoto@gmail.com>
2025-03-20 21:40:40 +0700

    asdsaascascaca

--- backend/server.js
+++ backend/server.js
@@ -20,18 +20,17 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-app.use("/api/products", productRoutes);
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/api/products", productRoutes);
 
 connectDB();
 
