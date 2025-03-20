92c6c741aa97a8bba7e25f61b7828749c238063d
AlfredhoPranoto <alfredhopranoto@gmail.com>
2025-03-20 20:52:21 +0700

    Lontong udah cape

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
   });
 }
 
